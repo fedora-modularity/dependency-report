d2e18383156eb45a7917364d583f785533dd042a
Adam Samalik <asamalik@redhat.com>
2017-08-04 19:39:45 +0200

    hardcode dependency definition

--- generate_lists.sh
+++ generate_lists.sh
@@ -5,3 +5,12 @@ for repo in $(cat module-repolist.txt); do
     echo "  Processing $repo repository"
     perl mklists.pl -r repos/$repo
 done
+
+for module in $(ls modules); do
+    touch modules/$module/modular-deps.txt
+done
+
+# Hardcoding module dependencies here.
+# FIXME: This needs to get figured out in a better way.
+echo httpd >> modules/freeipa/modular-deps.txt
+
