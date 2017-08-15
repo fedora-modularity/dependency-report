ac8b34537b09b6e2e21d5bd25ea0275341fe4b4b
Adam Samalik <asamalik@redhat.com>
2017-08-15 09:52:09 +0200

    add regenerate_everything.sh

---
+++ regenerate_everything.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+echo "\n========================================="
+echo "Cloning 'modularity-modules' repositories..."
+./clone_repos.sh
+
+echo "\n========================================="
+echo "Generating machine-readable top-lvl  package lists..."
+./generate_lists.sh
+
+echo "\n========================================="
+echo "Resolving runtime package dependencies using depchase..."
+./resolve_modules.sh
+
+echo "\n========================================="
+echo "Resolving runtime module dependencies..."
+make
+
+echo "\n========================================="
+echo "Resolving build package dependencies using depchase..."
+./resolve_build.sh
+
+echo "\n========================================="
+echo "Resolving build module dependencies..."
+./build_deps_definitions.sh
+
+echo "\n========================================="
+echo "Producing human-readable reports..."
+./reports.sh
+
+echo "\n========================================="
+echo "EVERYTHING is done! \\o/"
+echo "========================================="
+
