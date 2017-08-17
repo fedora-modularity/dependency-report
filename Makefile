cb17d8cf2956e1ea40c7c3a50ed3cb72ca371919
Adam Samalik <asamalik@redhat.com>
2017-08-17 16:50:41 +0200

    add many freeipa deps

--- Makefile
+++ Makefile
@@ -24,6 +24,24 @@ storage-devices: platform platform-placeholder
 
 platform-placeholder: platform
 
-freeipa: platform platform-placeholder java perl
+freeipa: platform platform-placeholder java perl 389-ds bind krb5 sssd samba pki tomcat resteasy httpd
 
 java: platform platform-placeholder
+
+389-ds: platform platform-placeholder
+
+bind: platform platform-placeholder
+
+krb5: platform platform-placeholder
+
+sssd: platform platform-placeholder
+
+samba: platform platform-placeholder
+
+pki: platform platform-placeholder
+
+tomcat: platform platform-placeholder
+
+resteasy: platform platform-placeholder
+
+httpd: platform platform-placeholder
--- build_deps_definitions.sh
+++ build_deps_definitions.sh
@@ -11,6 +11,15 @@
 ./build_deps.sh networking-base platform platform-placeholder
 ./build_deps.sh storage-devices platform platform-placeholder
 ./build_deps.sh platform-placeholder platform
-./build_deps.sh freeipa platform platform-placeholder
+./build_deps.sh freeipa platform platform-placeholder 389-ds
 ./build_deps.sh java        platform platform-placeholder
+./build_deps.sh 389-ds        platform platform-placeholder
+./build_deps.sh bind        platform platform-placeholder
+./build_deps.sh krb5        platform platform-placeholder
+./build_deps.sh sssd        platform platform-placeholder
+./build_deps.sh samba       platform platform-placeholder
+./build_deps.sh pki         platform platform-placeholder
+./build_deps.sh tomcat      platform platform-placeholder
+./build_deps.sh resteasy    platform platform-placeholder
+./build_deps.sh httpd       platform platform-placeholder
 
