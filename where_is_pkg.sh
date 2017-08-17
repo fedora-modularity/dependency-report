451afca756195ea834c4edc6045ac214fd76e8e0
Adam Samalik <asamalik@redhat.com>
2017-08-17 10:17:55 +0200

    fix all pkgs list

--- where_is_pkg.sh
+++ where_is_pkg.sh
@@ -2,13 +2,13 @@
 
 mkdir -p global_reports
 
-cat modules/*/*/runtime-binary-packages-short.txt  | sort | uniq -c | sort > global_reports/all-binary-pkgs-counted.txt
+cat modules/*/all/runtime-binary-packages-short.txt  | sort | uniq -c | sort > global_reports/all-binary-pkgs-counted.txt
 
 while read pkg_row; do
     pkg=$(echo $pkg_row | sed -e "s/.* //")
     printf "$pkg_row\t"
     for module in $(ls modules); do
-        cat modules/$module/*/runtime-binary-packages-short.txt | grep "^$pkg$" > /dev/null
+        cat modules/$module/all/runtime-binary-packages-short.txt | grep "^$pkg$" > /dev/null
         if [ $? -eq 0 ]; then
             printf "$module, "
         fi
