9ced9bc65ed9dab91164460360092e49d07f348a
Adam Samalik <asamalik@redhat.com>
2017-08-20 22:19:38 +0200

    exclude bootstrap from global reports

--- global_reports.sh
+++ global_reports.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
+modules=$(ls modules \
+    | sed \
+        -e "s/^bootstrap$//g")
+
 mkdir -p global_reports
 
 echo ""
 echo "Producing 'all-binary-pkgs' list..."
 
-cat modules/*/all/runtime-binary-packages-short.txt  | sort | uniq | sort > global_reports/all-binary-pkgs.txt
+for module in $modules; do
+    cat modules/$module/all/runtime-binary-packages-short.txt
+done | sort | uniq | sort > global_reports/all-binary-pkgs.txt
 
 echo ""
 echo "Producing 'all-binary-pkgs-counted' list..."
 
-cat modules/*/all/runtime-binary-packages-short.txt  | sort | uniq -c | sort > global_reports/all-binary-pkgs-counted.txt
+for module in $modules; do
+    cat modules/$modules/all/runtime-binary-packages-short.txt
+done | sort | uniq -c | sort > global_reports/all-binary-pkgs-counted.txt
 
 echo ""
 echo "Producing 'all-binary-pkgs-occurrences' list..."
@@ -18,7 +26,7 @@ echo "Producing 'all-binary-pkgs-occurrences' list..."
 while read pkg_row; do
     pkg=$(echo $pkg_row | sed -e "s/.* //")
     printf "$pkg_row\t"
-    for module in $(ls modules); do
+    for module in $modules; do
         cat modules/$module/all/runtime-binary-packages-short.txt | grep "^$pkg$" > /dev/null
         if [ $? -eq 0 ]; then
             printf "$module, "
@@ -30,12 +38,16 @@ done < global_reports/all-binary-pkgs.txt | column -ts $'\t' > global_reports/al
 echo ""
 echo "Producing 'missing-build-deps-binary-pkgs' list..."
 
-cat modules/*/all/buildtime-binary-packages-short.txt  | sort | uniq | sort > global_reports/missing-build-deps-binary-pkgs.txt
+for module in $modules; do
+    cat modules/$module/all/buildtime-binary-packages-short.txt
+done | sort | uniq | sort > global_reports/missing-build-deps-binary-pkgs.txt
 
 echo ""
 echo "Producing 'missing-build-deps-binary-pkgs-counted' list..."
 
-cat modules/*/all/buildtime-binary-packages-short.txt  | sort | uniq -c | sort > global_reports/missing-build-deps-binary-pkgs-counted.txt
+for module in $modules; do
+    cat modules/$module/all/buildtime-binary-packages-short.txt
+done | sort | uniq -c | sort > global_reports/missing-build-deps-binary-pkgs-counted.txt
 
 echo ""
 echo "Producing 'missing-build-deps-binary-pkgs-unmodularized' list..."
@@ -76,7 +88,7 @@ echo "Generating README in the global_reports directory..."
     echo ""
     echo "| Module | RPM compnents | RPMs missing in buildroot |"
     echo "|---|---|---|"
-    for module in $(ls modules); do
+    for module in $modules; do
         echo "| [**$module**](../modules/$module) | **$(cat modules/$module/all/runtime-binary-packages-short.txt | wc -l)** pkgs | [**$(cat modules/$module/all/buildtime-binary-packages-short.txt | wc -l)** pkgs missing](../modules/$module/all/buildtime-binary-packages-short.txt) |"
     done
 } > global_reports/README.md
